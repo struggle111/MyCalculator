c28b104db1eb6804acbc24aaad3245b3cad11bb6
struggle111 <1549183281@qq.com>
2015-05-16 14:56:08 +0800

    5-16  14:55

---
+++ app/src/main/java/com/example/shi_02/mycalculator/OperateFragment.java
@@ -0,0 +1,34 @@
+package com.example.shi_02.mycalculator;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by shi-02 on 2015/5/16.
+ */
+public class OperateFragment extends Fragment {
+
+    private View view;
+
+    private Context context;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.operator_layout,container,false);
+
+        initView();
+
+        return view;
+    }
+
+    private void initView(){
+
+        context = getActivity();
+    }
+}
